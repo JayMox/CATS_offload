fa1162d014c37285dd56c482f8ed4ae583908621
JayMox <jmoxley@mbayaq.org>
2018-01-29 11:24:52 -0800

    has adjustment for poor parsing of video trigger params in older gen tags

--- cats_md_scraper.R
+++ cats_md_scraper.R
@@ -12,10 +12,15 @@ depid <- "TOM_CC0705_20171005"
 (files <- list.files(dir, pattern=("*.txt|CFG"),full.names = T))
 
 #2017 metadata scraping, read in as character strings for regex
-txt <- sapply(read.delim(files[1]), "as.character")
+txt <- sapply(read.delim(files[3]), "as.character")
 #get field categories & delimit field names from field values
-labels <- c("[general]", txt[which(str_detect(txt, "\\[[:alpha:]*[:space:]?[:alpha:]*?\\]"))])
+labels <- c("[global]", txt[which(str_detect(txt, "\\[[:alpha:]*[:space:]?[:alpha:]*?\\]"))])
 fields <- data.frame(str_split(txt, "\\=", n = 2, simplify = T)) %>% #split names & fields
-  mutate(labels = labels[findInterval(seq(1:nrow(txt)), which(str_detect(txt, "\\[[:alpha:]*[:space:]?[:alpha:]*?\\]")))+1])
-
+  rename(field = X1, value = X2) %>% 
+  mutate(class = labels[findInterval(seq(1:nrow(txt)), which(str_detect(txt, "\\[[:alpha:]*[:space:]?[:alpha:]*?\\]")))+1]) %>% 
+  filter(!(str_detect(txt, "\\[[:alpha:]*[:space:]?[:alpha:]*?\\]")))
+#parse video trigger field in old gen tags that doesn't mine well
+idx <- str_detect(fields$field, "rawon")
+fields <- fields %>% mutate(field = ifelse(idx, str_replace(field, " rawon", ""), as.character(field)),
+                      value = ifelse(idx, str_c("rawon=", value), as.character(value)))
 
