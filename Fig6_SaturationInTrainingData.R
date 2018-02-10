c78670d478bccbf118631e6c6951f45c792ac594
JayMox <jmoxley@mbayaq.org>
2018-02-09 17:52:20 -0800

    data to crank through sims for each dataset.. needs plotting help.

--- Fig6_SaturationInTrainingData.R
+++ Fig6_SaturationInTrainingData.R
@@ -14,8 +14,7 @@ library(grid)
 dd <- "/Users/jmoxley/Documents/GitTank/CC_CamTags/data/"
 df <- read_csv(file.path(dd, "FinMountOrigChks_ODBA_test_obs_pred_1_17_hours.csv")) %>% 
   select(-X21) %>% filter(!is.na(depth))  #clip NAs to create a 6 hour test set using increasing amts of trianing data
-
-df <- df[,1:6]
+#df <- df[,1:6]   #minidataset
 #Goodness of Fit simulations
 window_max <- 10000 # in seconds
 low_win <- seq(from = 1, to = window_max,length.out = 50 )
@@ -71,9 +70,77 @@ for(j in 4:ncol(df)){
   colnames(saturation)[j-2] <- paste(colnames(df[,j]), "-sim", sep="");
   print(paste(j, "IS DONE"))
 }
-#save(saturation, file = file.path(dd, "saturaton_sims.RData"))
+save(saturation, file = file.path(dd, "fmo_sensitivity_sims.RData"))
 
 
+#####
+##other dataset
+#####
+#set drive
+dd <- "/Users/jmoxley/Documents/GitTank/CC_CamTags/data/"
+df <- read_csv(file.path(dd, "PR161108_ODBA_test_obs_pred_1_18_hours.csv")) %>% 
+  select(-X21) %>% filter(!is.na(depth))  #clip NAs to create a 6 hour test set using increasing amts of trianing data
+#df <- df[,1:6]   #minidataset
+#Goodness of Fit simulations
+window_max <- 10000 # in seconds
+low_win <- seq(from = 1, to = window_max,length.out = 50 )
+sims <- 100
+saturation = NULL;
+saturation <- data.frame(interval = rep(low_win, times = sims))
+
+for(j in 4:ncol(df)){
+  #subset data of interet
+  modeled <- df[,c(1,3,j)]  #time-sec, obsODBA, & predOBA w/ relevant training data
+  colnames(modeled) <- c("time.sec", "observed", "predicted")
+  
+  #clear storage space
+  metric_df_sim <- NULL
+  
+  #jackknife
+  for(x in 1:sims){
+    metric_df <- NULL
+    
+    for(i in 1:length(low_win)){
+      
+      # random number selection from which to start window grab
+      rand_num <- sample(1:15000,1)
+      print(rand_num)
+      
+      # acquire vectors describing that window based on indexed window size
+      # and starting point random-uniform drawing
+      x <- modeled$time.sec[rand_num:(rand_num+low_win[i])]
+      y_obs  <- modeled$observed[rand_num:(rand_num+low_win[i])]
+      y_pred <- modeled$predicted[rand_num:(rand_num+low_win[i])]
+      
+      # calculate area under the curve for actual and observed of the selected window
+      AUCobs  <- DescTools::AUC(x,y_obs)
+      AUCpred <- DescTools::AUC(x,y_pred)
+      
+      # two evaluation metrics, metric 1 appears sensitive to the magnitude of the AUC
+      #metric1 <- 1- ( abs( AUCobs - AUCpred )/ low_win[i] )
+      #metric2 <- ifelse(AUCobs > AUCpred, AUCpred/AUCobs, AUCobs/AUCpred)
+      metric3 <- 1 - abs( AUCobs - AUCpred )/ AUCobs  #error percentage
+      
+      # Build dataframe for one simulation
+      # DF: 1 - window size, 2 - metric1 , 3 - metric2
+      metric_df$window[i]  <- low_win[i]
+      metric_df$metric3[i] <- metric3
+    }  
+    
+    # Repeat X times and row bind the simulations
+    metric_df <- as.data.frame(metric_df)
+    metric_df_sim <- rbind(metric_df,metric_df_sim)
+    print(x) 
+  }
+  #store data
+  saturation[,j-2] <- metric_df_sim$metric3
+  colnames(saturation)[j-2] <- paste(colnames(df[,j]), "-sim", sep="");
+  print(paste(j, "IS DONE"))
+}
+save(saturation, file = file.path(dd, "pr116_sensitivity_sims.RData"))
+
+###############
+#attempt on plots
 #melt data 
 mdf <- reshape2::melt(saturation, id.vars = "interval")
 ggplot(mdf %>% filter(variable == "ODBA-pred-1hr-sim"),aes(x=interval,y=value,color=variable))+
