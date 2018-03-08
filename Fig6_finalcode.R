9966a41a8ac2f0ee762e3204e7d1d5af94426115
JayMox <jmoxley@mbayaq.org>
2018-03-07 18:21:31 -0800

    new plots for ms

--- Fig6_finalcode.R
+++ Fig6_finalcode.R
@@ -26,8 +26,9 @@ mdf <- melt(df, id.vars = c("interval", "id"))
 #plot (based on SO sol'n here: https://stackoverflow.com/questions/48835600/building-a-ggproto-geom-extension)
 
 #FIGURE CODE
-mdf <- mdf %>% mutate(label = factor(str_extract(variable, "[:digit:]{1,2}"), 
-                                     levels = as.character(seq(1:17))))
+mdf <- mdf %>% mutate(label = factor(paste(str_extract(variable, "[:digit:]{1,2}"), "hr"), 
+                                     levels = paste(seq(1:17), "hr")))
+#full scale
 ggplot(mdf, aes(x = interval, y = value, group=label, color=label)) + 
   geom_point(color = "light gray", alpha = 0.2) +
   lapply(1:10, # NUMBER OF LOESS
@@ -36,8 +37,24 @@ ggplot(mdf, aes(x = interval, y = value, group=label, color=label)) +
                                        2000),  #NUMBER OF POINTS TO SAMPLE
                                 ], se=FALSE, span = .95, size = 0.2, method = "loess")
          }) +
-  facet_grid(variable~id) + themeo +   guides(color = F) +
-  scale_x_continuous(expand=c(0,0)) + scale_y_continuous(limits = c(0.5, 1.0), breaks=c(0.5, 0.75, 1.0))
+  facet_grid(label~id) + themeo +   guides(color = F) +
+  labs(x = "AUC Interval (sec x 10^2)", y = "Accuracy (inferred from error %age") + 
+  scale_x_continuous(expand=c(0,0), breaks = c(2500, 5000, 7500, 10000), labels = c(25,50,75,100)) + scale_y_continuous(limits = c(0.5, 1.0), breaks=c(0.5, 0.75, 1.0))
+
+
+#filtered down to 3x2
+ggplot(mdf %>% filter(label %in% c("1 hr","4 hr","7 hr")), aes(x = interval, y = value, group=label, color=label)) + 
+  geom_point(color = "light gray", alpha = 0.2) +
+  lapply(1:10, # NUMBER OF LOESS
+         function(i) {
+           df <- mdf %>% filter(label %in% c("1 hr", "4 hr", "7 hr"))
+           geom_smooth(data=df[sample(1:nrow(df), 
+                                       2000),  #NUMBER OF POINTS TO SAMPLE
+                                ], se=FALSE, span = .95, size = 0.2, method = "loess")
+         }) +
+  facet_grid(label~id) + themeo +   guides(color = F) +
+  labs(x = "AUC Interval (sec x 10^2)", y = "Accuracy (inferred from error %age") + 
+  scale_x_continuous(expand=c(0,0), breaks = c(2500, 5000, 7500, 10000), labels = c(25,50,75,100)) + scale_y_continuous(limits = c(0.5, 1.0), breaks=c(0.5, 0.75, 1.0))
 
 
 ###
