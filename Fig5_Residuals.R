7773e9b3008dfdc22438625eeece2727ba4d2d1f
JayMox <jmoxley@mbayaq.org>
2018-05-25 17:28:09 -0700

    fig5 plotting semi finalized

--- Fig5_Residuals.R
+++ Fig5_Residuals.R
@@ -23,35 +23,64 @@ themeo <-theme_classic()+
 dd <- "/Users/jmoxley/Dropbox (MBA)/NN_processing/data/ANN_swap"
 #get data
 fmo.swap <- read.csv(file.path(dd, "FinMountOrigChks_ODBA_test_obs_pred_1_17_hours_SWAP.csv"), header = T) %>% 
-  select(time.sec, depth, observed = ODBA.obs, ODBA.pred.3hr) %>% 
+  select(time.sec, depth, obs = ODBA.obs, pred = ODBA.pred.3hr) %>% 
   filter(!is.na(time.sec)) %>% 
-  mutate(id = "FMO", tuning = "swapped", error = observed - ODBA.pred.3hr)
+  mutate(id = "fmo", shark = "Shark 2", tuning = "swapped") %>% #add fields
+  mutate(obs.sc = obs/median(obs), pred.sc = pred/median(pred), obs.sc2 = obs/10, pred.sc2 = pred/10) %>% #normalize
+  mutate(error = obs - pred, error.sc = obs.sc - pred.sc, error.sc2 = obs.sc2 - pred.sc2)
 pr.swap <- read.csv(file.path(dd, "PR161108_ODBA_test_obs_pred_1_18_hours_SWAP.csv"), header = T) %>% 
-  select(time.sec, depth, observed = ODBA.obs, ODBA.pred.3hr) %>% 
+  select(time.sec, depth, obs = ODBA.obs, pred = ODBA.pred.3hr) %>% 
   filter(!is.na(time.sec)) %>% 
-  mutate(id = "PR", tuning = "swapped", error = observed - ODBA.pred.3hr)
+  mutate(id = "pr", shark = "Shark 1", tuning = "swapped") %>% #add fields
+  mutate(obs.sc = obs/median(obs), pred.sc = pred/median(pred), obs.sc2 = obs, pred.sc2 = pred) %>% #normalize
+  mutate(error = obs - pred, error.sc = obs.sc - pred.sc, error.sc2 = obs.sc2 - pred.sc2)
 fmo.native <- read.csv(file.path(substr(dd, 0, 47), "FinMountOrigChks_ODBA_test_obs_pred_1_17_hours.csv"), header = T) %>% 
-  select(time.sec, depth, observed = ODBA.obs, ODBA.pred.3hr) %>% 
-  filter(!is.na(time.sec)) %>% 
-  mutate(id = "FMO", tuning = "native", error = observed - ODBA.pred.3hr) 
+  select(time.sec, depth, obs = ODBA.obs, pred = ODBA.pred.3hr) %>% 
+  filter(!is.na(time.sec)) %>%
+  mutate(id = "fmo", shark = "Shark 2", tuning = "native") %>% #add fields
+  mutate(obs.sc = obs/median(obs), pred.sc = pred/median(pred), obs.sc2 = obs/10, pred.sc2 = pred/10) %>% #normalize
+  mutate(error = obs - pred, error.sc = obs.sc - pred.sc, error.sc2 = obs.sc2 - pred.sc2)
 pr.native <- read.csv(file.path(substr(dd, 0, 48), "PR161108_ODBA_test_obs_pred_1_18_hours.csv"), header = T) %>% 
-  select(time.sec, depth, observed = ODBA.obs, ODBA.pred.3hr) %>% 
-  filter(!is.na(time.sec)) %>% 
-  mutate(id = "PR", tuning = "native", error = observed - ODBA.pred.3hr)
+  select(time.sec, depth, obs = ODBA.obs, pred = ODBA.pred.3hr) %>% 
+  filter(!is.na(time.sec)) %>%
+  mutate(id = "pr", shark = "Shark 1", tuning = "native") %>% #add fields
+  mutate(obs.sc = obs/median(obs), pred.sc = pred/median(pred), obs.sc2 = obs, pred.sc2 = pred) %>% #normalize
+  mutate(error = obs - pred, error.sc = obs.sc - pred.sc, error.sc2 = obs.sc2 - pred.sc2)
 df <- bind_rows(fmo.swap, pr.swap, fmo.native, pr.native)
 
 #plotting
-p <- ggplot(data = df, aes(x = error)) + #geom_histogram(aes(y = ..density.., fill = fct_rev(tuning), alpha = 0.3)) + 
+p <- ggplot(data = df %>% select(-error, -error.sc, -obs.sc, -pred.sc, -obs.sc2, -pred.sc2) %>% 
+       gather(source, odba, -time.sec, -depth, -id, -tuning)) + geom_line(aes(x = time.sec, y = odba, color = source, linetype = tuning)) + 
+  facet_wrap(~id) + themeo+ geom_hline(yintercept = 0) + labs(title = "non-normalized")
+q <- ggplot(data = df %>% select(-error, -error.sc, -obs, -pred, -obs.sc2, -pred.sc2) %>% 
+              gather(source, odba, -time.sec, -depth, -id, -tuning)) + geom_line(aes(x = time.sec, y = odba, color = source, linetype = tuning)) + 
+  facet_wrap(~id) + themeo + geom_hline(yintercept = 0) + labs(title = "normalized from median")
+s <- ggplot(data = df %>% select(-error, -error.sc, -obs, -pred, -obs.sc, -pred.sc) %>% 
+              gather(source, odba, -time.sec, -depth, -id, -tuning)) + geom_line(aes(x = time.sec, y = odba, color = source, linetype = tuning)) + 
+  facet_wrap(~id) + themeo + geom_hline(yintercept = 0) + labs(title = "fmo/10")
+grid.arrange(p, q, s)
+
+#plotting
+(p <- ggplot(data = df, aes(x = error.sc)) + #geom_histogram(aes(y = ..density.., fill = fct_rev(tuning), alpha = 0.3)) + 
   geom_density(alpha = 0.5, aes(fill = fct_rev(tuning)))+
   geom_vline(xintercept = 0, linetype = "dashed")+
-  facet_wrap(~id, scales = "free") + 
-  themeo
+  scale_fill_manual(values = c("#e11f28", "#397fba")) + 
+  scale_x_continuous(limits = c(-2,2))+
+  facet_wrap(~shark, scales = "free") + guides(fill = F) + labs(x = "residuals", y = "") + 
+  themeo)
 
 #ts
 #quick look
-chk_plot <- ggplot(data = df %>% select(-error) %>% spread(tuning, ODBA.pred.3hr), aes(x=time.sec)) +
-    geom_line(aes(y=observed), color = "black", size = 0.2) + 
-    geom_line(aes(y=native), color = "#397fba", size = 0.2)+
-    geom_line(aes(y=swapped), color = "#e11f28", size = 0.2)+
-    labs(title = "green = observed, red = native, blue = generalized") + themeo + facet_wrap(~id, scales = "free")
-ggplotly(chk_plot)
+chk_plot <- ggplot(data = df %>% select(time.sec, depth, obs.sc, shark, id, tuning, pred.sc) %>% spread(tuning, pred.sc) %>% 
+                     filter(time.sec > 79000 & time.sec < 82500), aes(x=time.sec)) +
+    geom_line(aes(y=obs.sc), color = "black", size = 0.3) + 
+    geom_line(aes(y=native), color = "#397fba", size = 0.3)+    #blue
+    geom_line(aes(y=swapped), color = "#e11f28", size = 0.35)+  #red
+    scale_y_continuous(limits = c(0, 5)) + 
+    scale_x_continuous(labels = NULL)+
+    labs(x = "time", y = "ODBA (standardized)", title = "black = observed, blue = native, red = generalized") + 
+  themeo + facet_wrap(~shark)
+#ggplotly(chk_plot)
+chk_plot
+
+grid.arrange(p, chk_plot, ncol = 1)
